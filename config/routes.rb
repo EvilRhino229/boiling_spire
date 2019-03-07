be987bb2beaa5cd91836dfa33e614196b7b744a4
user <user@users-MacBook-Pro-4.local>
2019-03-06 19:56:20 -0600

    i ungoofed stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   get '/portfolio' => 'static_pages#portfolio'
   get '/contact' => 'static_pages#contact'
+  get '/sup' => 'static_pages#sup'
 end
