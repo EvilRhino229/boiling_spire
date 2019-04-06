a9c54d1115b3c3385e17ef9efcd9e5f95e320d34
user <user@users-MacBook-Pro-4.local>
2019-04-06 16:15:03 -0500

    Validated my Heroku, boi

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 # Mailer
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['myleviwj5500'],
+  :password => ENV['app126863143@heroku.com'],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
